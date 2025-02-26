a6022c837154c27f608b3afc413e7223b426640d
jegorix <0311egor@gmail.com>
2025-02-26 19:45:39 +0300

    add class Menu

--- task_manager.py
+++ task_manager.py
@@ -1,2 +1,42 @@
-##TaskManager
-print("TaskManager")
+
+class Menu:
+    def __init__(self):
+        self.actions = {
+            '1': "Добавить задачу",
+            '2': "Показать список задач",
+            '3': "Выйти"
+        }
+
+    def show_actions(self):
+        for (key,value) in self.actions.items():
+            print(f"{key}. {value}")
+
+    def choice(self, action):
+        if action in self.actions:
+            print(f"Вы выбрали: {self.actions[action]}")
+            if action == '3':
+                print("Выход...")
+                return False
+        else:
+            print("Неизвестная команда. Попробуйте снова.")
+
+
+
+
+
+
+def main():
+    menu = Menu()
+    while True:
+        print("Выберите действие:")
+        menu.show_actions()
+        user_action = input("Введите номер: ")
+
+        if not menu.choice(user_action):
+            break
+
+
+
+
+if __name__ == '__main__':
+    main()
