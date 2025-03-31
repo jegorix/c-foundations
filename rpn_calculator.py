78fa24d16d9533d496800609ec310724dfb05f9a
jegorix <0311egor@gmail.com>
2025-03-31 18:26:28 +0300

    make reverse polish notation

---
+++ rpn_calculator.py
@@ -0,0 +1,52 @@
+
+def binary_op(func):
+    def redef(stack):
+        b = stack.pop()
+        a = stack.pop()
+        result = func(a, b)
+        stack.append(result)
+        return result
+    return redef
+
+
+
+
+
+def execute_program(program: str):
+    stack = []
+    for token in program.split():
+        if token == '+':
+            if len(stack) < 2:
+                raise Exception(f'Not enough operands(now = {len(stack)})')
+
+            b = stack.pop()
+            a = stack.pop()
+            result = a + b
+            stack.append(result)
+
+        elif token == '-':
+            if len(stack) < 2:
+                raise Exception(f'Not enough operands(now = {len(stack)})')
+
+            b = stack.pop()
+            a = stack.pop()
+            result = a - b
+            stack.append(result)
+
+        else:
+            try:
+                stack.append(int(token))
+            except ValueError:
+                print(f"{token!r} is unknown symbol")
+
+    if len(stack) != 1:
+        raise("After calculation exists more than one number")
+
+    return stack.pop()
+
+
+
+print(execute_program('5 6 7 + -'))
+
+
+
