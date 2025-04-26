5f1529d20fdfc04f69a29fa5647b457736c94c14
jegorix <0311egor@gmail.com>
2025-04-26 13:58:26 +0300

    create task manger preview menu

---
+++ upozn/operating_mode.c
@@ -0,0 +1,44 @@
+//
+// Created by jegorix on 26.04.25.
+//
+
+#include "operating_mode.h"
+#include "validators.h"
+
+void operating_mode_menu()
+{
+    int running = 1;
+
+    while(running) {
+        printf("\n╔══════════════════════════════╗\n");
+        printf("║            МЕНЮ              ║\n");
+        printf("╠══════════════════════════════╣\n");
+        printf("║ 1. Создать новый ежедневник  ║\n");
+        printf("║ 2. Загрузить задачи из файла ║\n");
+        printf("║ 3. Выход в главное меню      ║\n");
+        printf("╚══════════════════════════════╝\n");
+        printf("\n╭──────────────────────────────╮\n");
+        printf("│Введите команду:              │\n");
+        printf("╰─> ");
+        int choice = execute_verification(1, 3);
+        switch (choice)
+        {
+            case 1:
+//                task_manager_create();
+                break;
+
+            case 2:
+                break;
+
+            case 3:
+                printf("\nВыход в главное меню...\n");
+                running = 0;
+                return;
+
+
+        }
+
+    }
+
+
+}
---
+++ upozn/operating_mode.h
@@ -0,0 +1,10 @@
+//
+// Created by jegorix on 26.04.25.
+//
+
+#ifndef UPOZN_OPERATING_MODE_H
+#define UPOZN_OPERATING_MODE_H
+#include <stdio.h>
+#include "validators.h"
+void operating_mode_menu();
+#endif //UPOZN_OPERATING_MODE_H
